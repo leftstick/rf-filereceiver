cdc548dbd7b568b817cddce373a3425007680ee2
Howard.Zuo <leftstick@qq.com>
2019-04-18 17:49:09 +0800

    refactor: upgrade with new version of umi-library

--- .umirc.library.js
+++ .umirc.library.js
@@ -4,11 +4,9 @@ export default {
   esm: 'rollup',
   cjs: 'rollup',
   doc: {
-    public: './docs/public',
+    public: './public',
     src: './docs',
-    hashRouter: true,
-    themeConfig: { mode: 'light' },
-    base: '/rf-filereceiver',
+    base: '/rf-filereceiver/',
     title: 'rf-filereceiver',
     description: 'A component which interacts with user click, and download file via downloadjs',
     plugins: [splash()]
