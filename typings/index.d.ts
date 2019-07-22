c48f762f7f774a40f1a85d69345e41b066c51bbe
Howard.Zuo <leftstick@qq.com>
2019-07-22 17:33:49 +0800

    faat: new props of onProgress

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,6 +1,12 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 
+interface ProgressEvent {
+  loaded: number
+  total: number
+  complete: number
+}
+
 interface IPropsType {
   children: JSX.Element
   url: string | (() => string)
@@ -9,6 +15,7 @@ interface IPropsType {
   }
   fileName: string
   fileMIMEType: string
+  onProgress: (event: ProgressEvent) => void
 }
 declare class RfReceiver extends React.Component<IPropsType> {
   _fireOriginalClick: (e: MouseEvent) => void
